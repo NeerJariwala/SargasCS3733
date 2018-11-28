5ff1805cc6bc60419258ad6b7c64187d8780a234
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2018-11-28 09:02:28 -0500

    started part the parses request

--- GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/CreateScheduleHandler.java
+++ GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/CreateScheduleHandler.java
@@ -1,10 +1,12 @@
-package edu.wpi.sargas.demo.lambdas;
+package edu.wpi.sargas.demo.http_responses_and_requests;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -14,8 +16,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.google.gson.Gson;
-
-import edu.wpi.sargas.demo.http_responses_and_requests.CreateScheduleResponse;
 
 public class CreateScheduleHandler implements RequestStreamHandler {
 	
@@ -62,8 +62,37 @@ public class CreateScheduleHandler implements RequestStreamHandler {
     	
     	if(!processed) { 
     		//if it's not OPTIONS and not a parse exception, further processing must be done
+    		boolean invalidInput = false;
     		
+    		CreateScheduleRequest request = new Gson().fromJson(httpBody,CreateScheduleRequest.class);
+    		//get the request in the form of a class
+    		String name = request.name;
+    		int duration = request.duration;
     		
+    		try {
+    			LocalDate sd = LocalDate.parse(request.startDate);
+    			LocalDate ed = LocalDate.parse(request.endDate);
+    		} catch(DateTimeParseException e) {
+    			logger.log(e.toString());
+        		httpResponse = new CreateScheduleResponse(400, null, null);
+        		jsonResponse.put("body", new Gson().toJson(httpResponse));
+        		invalidInput = true;
+    		}
+    		//TODO: Above only works for date format YYYY-MM-DD. Ensure that format is used in the requests
+    		int startHour = request.startHour;
+    		int endHour = request.endHour;
+    		
+    		if(startHour < 0 || startHour > 23 || endHour <= startHour || endHour > 23) {
+    			//if hour input is invalid
+    			logger.log("Invalid hours");
+    			httpResponse = new CreateScheduleResponse(400, null, null);
+        		jsonResponse.put("body", new Gson().toJson(httpResponse));
+        		invalidInput = true;
+    		}
+    		
+    		if(!invalidInput) {
+    			
+    		}
     		
     	}
     	
