13c76ed8013cc1d2d9474455fe3ffb1661643f63
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2018-11-28 08:36:19 -0500

    Wrote CreateScheduleHandler case for options response, parseException

--- GP/src/main/java/edu/wpi/sargas/demo/lambdas/CreateScheduleHandler.java
+++ GP/src/main/java/edu/wpi/sargas/demo/lambdas/CreateScheduleHandler.java
@@ -1,7 +1,9 @@
 package edu.wpi.sargas.demo.lambdas;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import org.json.simple.JSONObject;
@@ -26,13 +28,36 @@ public class CreateScheduleHandler implements RequestStreamHandler {
     	
     	JSONObject jsonResponse = new JSONObject();
     	CreateScheduleResponse httpResponse = null;
+    	String httpBody = null;
+    	boolean processed = false;
     	
     	try {
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+    		JSONParser parser = new JSONParser();
+    		JSONObject jsonRequest = (JSONObject)parser.parse(reader);
+    		//parse the request
     		
+    		logger.log("Input was " + jsonRequest.toJSONString());
+    		String requestType = (String)jsonRequest.get("httpMethod");
+    		
+    		if(requestType != null && requestType.equalsIgnoreCase("OPTIONS")) {
+    			//if we got an OPTIONS request, provide a 200 response
+    			logger.log("Options request");
+    			httpResponse = new CreateScheduleResponse(200, null, null);
+    			//no schedule/secret code needed because it was an options request
+    			
+    		} else { //otherwise, check out the response body later
+    			httpBody = (String)jsonRequest.get("body");
+    			if(httpBody == null) {
+    				httpBody = jsonRequest.toJSONString();
+    			}
+    		}
     	} catch(ParseException e) {
+    		//if unable to parse, prepare an invalid input response
     		logger.log(e.toString());
     		httpResponse = new CreateScheduleResponse(400, null, null);
     		jsonResponse.put("body", new Gson().toJson(httpResponse));
+    		processed = true;
     	}
     	
     }
