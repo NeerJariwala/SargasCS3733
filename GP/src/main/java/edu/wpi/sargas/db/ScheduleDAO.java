a40b1c1c1eea1fa4c572633c248b6389c098b54c
japierre <jean-philippe@130.215.9.160>
2018-11-29 12:21:14 -0500

    Had a import to a different git

--- GP/src/main/java/edu/wpi/sargas/db/ScheduleDAO.java
+++ GP/src/main/java/edu/wpi/sargas/db/ScheduleDAO.java
@@ -5,7 +5,6 @@ import java.sql.ResultSet;
 import java.util.UUID;
 import java.sql.Date;
 
-import edu.wpi.cs.heineman.model.Constant;
 import edu.wpi.sargas.db.DatabaseUtil;
 import edu.wpi.sargas.demo.entity.Schedule;
 
