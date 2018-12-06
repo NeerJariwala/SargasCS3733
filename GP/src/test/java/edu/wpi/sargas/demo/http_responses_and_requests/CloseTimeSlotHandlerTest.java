96858572378b51d982d57c1c6a08dd7ccd3cfdad
Mari1 <Mari1@130.215.237.206>
2018-12-05 21:51:23 -0500

    CloseTimeSlothandler Test

---
+++ GP/src/test/java/edu/wpi/sargas/demo/http_responses_and_requests/CloseTimeSlotHandlerTest.java
@@ -0,0 +1,109 @@
+package edu.wpi.sargas.demo.http_responses_and_requests;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.LocalDate;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import edu.wpi.sargas.db.MeetingDAO;
+import edu.wpi.sargas.db.ScheduleDAO;
+import edu.wpi.sargas.demo.TestContext;
+import edu.wpi.sargas.demo.entity.Schedule;
+import edu.wpi.sargas.demo.entity.Meeting;
+import edu.wpi.sargas.demo.http_responses_and_requests.CreateScheduleHandler;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class CloseTimeSlotHandlerTest {
+	
+	Context createContext(String apiCall) {
+        TestContext ctx = new TestContext();
+        ctx.setFunctionName(apiCall);
+        return ctx;
+    }
+	
+    static String SAMPLE_INPUT_STRING = "{\"secretCode\": ";
+
+    @Test
+    public void testCloseTimeSlotHandler() throws IOException {
+    	ScheduleDAO dao = new ScheduleDAO();
+    	Schedule sched = null;
+    	
+    	try {
+    		sched = new Schedule(20,"name",LocalDate.of(2000, 1, 1), LocalDate.of(2000, 1, 3), 4,16);
+    	} catch(Exception e) {
+    		System.out.println("problem");
+    	}
+    	
+        CloseTimeSlotHandler handler = new CloseTimeSlotHandler();
+        
+        String testInput = SAMPLE_INPUT_STRING + sched.getSecretCode() + "\"}";
+        System.out.println(testInput);
+        
+        InputStream input = new ByteArrayInputStream(testInput.getBytes());;
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, createContext("random"));
+        
+        String sampleOutputString = output.toString();
+        System.out.println(sampleOutputString);
+        JSONObject response = null;
+        JSONObject body = null;
+        try {
+        	response = (JSONObject)new JSONParser().parse(sampleOutputString);
+        	body = (JSONObject)new JSONParser().parse(response.get("body").toString());
+        } catch(ParseException e) {
+        	System.out.println("problem");
+        }
+        
+        Assert.assertEquals(body.get("httpCode").toString(), "200");
+        
+    }
+    
+    @Test
+    public void testError() throws IOException {
+    	ScheduleDAO dao = new ScheduleDAO();
+    	Schedule sched = null;
+    	
+    	
+    	try {
+    		sched = new Schedule(20,"name",LocalDate.of(2000, 1, 1), LocalDate.of(2000, 1, 3), 4,16);
+    	} catch(Exception e) {
+    		System.out.println("problem");
+    	}
+        CloseTimeSlotHandler handler = new CloseTimeSlotHandler();
+        
+        String testInput = SAMPLE_INPUT_STRING + "does not exist" + "\"}";
+        System.out.println(testInput);
+        
+        InputStream input = new ByteArrayInputStream(testInput.getBytes());;
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, createContext("random"));
+        
+        String sampleOutputString = output.toString();
+        System.out.println(sampleOutputString);
+        JSONObject response = null;
+        JSONObject body = null;
+        try {
+        	response = (JSONObject)new JSONParser().parse(sampleOutputString);
+        	body = (JSONObject)new JSONParser().parse(response.get("body").toString());
+        } catch(ParseException e) {
+        	System.out.println("problem");
+        }
+        
+        Assert.assertEquals(body.get("httpCode").toString(), "400");
+        
+    }
+}
