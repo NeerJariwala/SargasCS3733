ccd5f0839b36f24d5bdd1ef85d6db162689658c9
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2018-12-07 15:51:28 -0500

    Started ParticipantCancelMeetingHandler

---
+++ GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/ParticipantCancelMeetingResponse.java
@@ -0,0 +1,11 @@
+package edu.wpi.sargas.demo.http_responses_and_requests;
+
+public class ParticipantCancelMeetingResponse {
+	
+	int httpCode;
+	
+	public ParticipantCancelMeetingResponse(int code) {
+		httpCode = code;
+	}
+	
+}
