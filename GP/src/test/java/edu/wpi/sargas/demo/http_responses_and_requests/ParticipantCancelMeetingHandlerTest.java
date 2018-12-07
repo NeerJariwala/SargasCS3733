848a2e456a26a0c655d289416f5ec9e9e978f34b
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2018-12-07 16:14:42 -0500

    Wrote test cases for ParticipantCancelMeetingHandler

--- GP/src/test/java/edu/wpi/sargas/demo/http_responses_and_requests/ParticipantCancelMeetingHandlerTest.java
+++ GP/src/test/java/edu/wpi/sargas/demo/http_responses_and_requests/ParticipantCancelMeetingHandlerTest.java
@@ -5,30 +5,112 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.LocalDate;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import edu.wpi.sargas.db.MeetingDAO;
+import edu.wpi.sargas.db.ScheduleDAO;
+import edu.wpi.sargas.demo.TestContext;
+import edu.wpi.sargas.demo.entity.Meeting;
+import edu.wpi.sargas.demo.entity.Schedule;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
 public class ParticipantCancelMeetingHandlerTest {
-
-    private static final String SAMPLE_INPUT_STRING = "{\"foo\": \"bar\"}";
-    private static final String EXPECTED_OUTPUT_STRING = "{\"FOO\": \"BAR\"}";
+	
+	Context createContext(String apiCall) {
+        TestContext ctx = new TestContext();
+        ctx.setFunctionName(apiCall);
+        return ctx;
+    }
+	
+    static String SAMPLE_INPUT_STRING = "{\"secretCode\": ";
 
     @Test
-    public void testParticipantCancelMeetingHandler() throws IOException {
+    public void testOrganizerCancelMeetingHandler() throws IOException {
+    	ScheduleDAO dao = new ScheduleDAO();
+    	Schedule sched = null;
+    	
+    	try {
+    		sched = new Schedule(60,"name",LocalDate.of(2000, 1, 1), LocalDate.of(2000, 1, 3), 4,16);
+    	} catch(Exception e) {
+    		System.out.println("problem");
+    	}
+    	Meeting meeting = new Meeting("meeting", sched.weeks.get(0).days.get(0).timeslots.get(0).timeslotID);
+    	
+    	try {
+    		new MeetingDAO().createMeeting(meeting);
+    	} catch(Exception e) {
+    		System.out.println("problem");
+    	}
+    	
         ParticipantCancelMeetingHandler handler = new ParticipantCancelMeetingHandler();
-
-        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());;
+        
+        String testInput = SAMPLE_INPUT_STRING + "\"" + meeting.secretCode + "\"}";
+        System.out.println(testInput);
+        
+        InputStream input = new ByteArrayInputStream(testInput.getBytes());;
         OutputStream output = new ByteArrayOutputStream();
 
-        handler.handleRequest(input, output, null);
-
-        // TODO: validate output here if needed.
+        handler.handleRequest(input, output, createContext("random"));
+        
         String sampleOutputString = output.toString();
         System.out.println(sampleOutputString);
-        Assert.assertEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+        JSONObject response = null;
+        JSONObject body = null;
+        try {
+        	response = (JSONObject)new JSONParser().parse(sampleOutputString);
+        	body = (JSONObject)new JSONParser().parse(response.get("body").toString());
+        } catch(ParseException e) {
+        	System.out.println("problem");
+        }
+        
+        Assert.assertEquals(body.get("httpCode").toString(), "200");
+        
+    }
+    
+    @Test
+    public void testError() throws IOException {
+    	ScheduleDAO dao = new ScheduleDAO();
+    	Schedule sched = null;
+    	
+    	
+    	try {
+    		sched = new Schedule(20,"name",LocalDate.of(2000, 1, 1), LocalDate.of(2000, 1, 3), 4,16);
+    	} catch(Exception e) {
+    		System.out.println("problem");
+    	}
+    	Meeting meeting = new Meeting("meeting", sched.weeks.get(0).days.get(0).timeslots.get(0).timeslotID);
+        ParticipantCancelMeetingHandler handler = new ParticipantCancelMeetingHandler();
+        
+        String testInput = SAMPLE_INPUT_STRING + "\"" + "does not exist" + "\"}";
+        System.out.println(testInput);
+        
+        InputStream input = new ByteArrayInputStream(testInput.getBytes());;
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, createContext("random"));
+        
+        String sampleOutputString = output.toString();
+        System.out.println(sampleOutputString);
+        JSONObject response = null;
+        JSONObject body = null;
+        try {
+        	response = (JSONObject)new JSONParser().parse(sampleOutputString);
+        	body = (JSONObject)new JSONParser().parse(response.get("body").toString());
+        } catch(ParseException e) {
+        	System.out.println("problem");
+        }
+        
+        Assert.assertEquals(body.get("httpCode").toString(), "400");
+        
     }
 }
