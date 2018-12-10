f2f092c0cb1881de1cc48993553fd6af595c2548
Mari1 <Mari1@130.215.237.206>
2018-12-10 10:23:53 -0500

    changed showschedulehandler back

--- GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/ShowScheduleHandler.java
+++ GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/ShowScheduleHandler.java
@@ -82,7 +82,7 @@ public class ShowScheduleHandler implements RequestStreamHandler {
     		ShowScheduleRequest request = new Gson().fromJson(httpBody,ShowScheduleRequest.class);
     		//get the request in the form of a class
     		Schedule sched = null;
-    		LocalDate date = LocalDate.of(2018, 12, 31);
+    		LocalDate date = null;
     		
     		try {
     			date = LocalDate.parse(request.date);
