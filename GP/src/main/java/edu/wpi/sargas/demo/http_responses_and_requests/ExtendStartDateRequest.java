0cf9356b567e846f1e07c30f32034fd7cad78067
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2018-12-11 10:50:27 -0500

    Started extendStartDate, made slight edit to generateWeeks in Schedule

---
+++ GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/ExtendStartDateRequest.java
@@ -0,0 +1,6 @@
+package edu.wpi.sargas.demo.http_responses_and_requests;
+
+public class ExtendStartDateRequest {
+	String secretCode; //secret code of schedule to change
+	String newStart; //new start date
+}
---
+++ GP/src/main/java/edu/wpi/sargas/demo/http_responses_and_requests/ExtendStartDateResponse.java
@@ -0,0 +1,10 @@
+package edu.wpi.sargas.demo.http_responses_and_requests;
+
+public class ExtendStartDateResponse {
+	int httpCode;
+	
+	public ExtendStartDateResponse(int httpCode) {
+		this.httpCode = httpCode;
+	}
+	
+}
