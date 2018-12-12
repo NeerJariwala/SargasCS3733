8b9c4ce9f189352b1345830b45a6610f89a9174a
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2018-12-11 19:41:58 -0500

    Wrote test cases for extendStartDate

--- GP/src/test/java/edu/wpi/sargas/demo/http_responses_and_requests/ExtendStartDateHandlerTest.java
+++ GP/src/test/java/edu/wpi/sargas/demo/http_responses_and_requests/ExtendStartDateHandlerTest.java
@@ -5,30 +5,98 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.LocalDate;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import edu.wpi.sargas.demo.entity.Schedule;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
 public class ExtendStartDateHandlerTest {
 
-    private static final String SAMPLE_INPUT_STRING = "{\"foo\": \"bar\"}";
-    private static final String EXPECTED_OUTPUT_STRING = "{\"FOO\": \"BAR\"}";
 
     @Test
     public void testExtendStartDateHandler() throws IOException {
         ExtendStartDateHandler handler = new ExtendStartDateHandler();
-
-        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());;
+        
+        ExtendStartDateRequest request = new ExtendStartDateRequest();
+        
+        Schedule sched = null;
+        
+        try {
+        	sched = new Schedule(60, "Test",LocalDate.of(2001, 1, 1), LocalDate.of(2001, 1, 2),5,6);
+        } catch(Exception e) {
+        	System.out.println(e.toString());
+        }
+        
+        request.secretCode = sched.getSecretCode();
+        request.newStart = "2000-12-30";
+        String jsonRequest = new Gson().toJson(request);
+        
+        InputStream input = new ByteArrayInputStream(jsonRequest.getBytes());;
         OutputStream output = new ByteArrayOutputStream();
 
         handler.handleRequest(input, output, null);
-
-        // TODO: validate output here if needed.
+        
         String sampleOutputString = output.toString();
         System.out.println(sampleOutputString);
-        Assert.assertEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+        JSONObject response = null;
+        JSONObject body = null;
+        try {
+        	response = (JSONObject)new JSONParser().parse(sampleOutputString);
+        	body = (JSONObject)new JSONParser().parse(response.get("body").toString());
+        } catch(ParseException e) {
+        	System.out.println("problem");
+        }
+        
+        Assert.assertEquals(body.get("httpCode").toString(), "200");
+        
     }
+    
+    @Test
+    public void test2() throws IOException {
+        ExtendStartDateHandler handler = new ExtendStartDateHandler();
+        
+        ExtendStartDateRequest request = new ExtendStartDateRequest();
+        
+        Schedule sched = null;
+        
+        try {
+        	sched = new Schedule(60, "Test",LocalDate.of(2001, 1, 1), LocalDate.of(2001, 1, 5),5,6);
+        } catch(Exception e) {
+        	System.out.println(e.toString());
+        }
+        
+        request.secretCode = sched.getSecretCode();
+        request.newStart = "2001-01-02";
+        String jsonRequest = new Gson().toJson(request);
+        
+        InputStream input = new ByteArrayInputStream(jsonRequest.getBytes());;
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, null);
+        
+        String sampleOutputString = output.toString();
+        System.out.println(sampleOutputString);
+        JSONObject response = null;
+        JSONObject body = null;
+        try {
+        	response = (JSONObject)new JSONParser().parse(sampleOutputString);
+        	body = (JSONObject)new JSONParser().parse(response.get("body").toString());
+        } catch(ParseException e) {
+        	System.out.println("problem");
+        }
+        
+        Assert.assertEquals(body.get("httpCode").toString(), "400");
+        
+    }
+    
 }
